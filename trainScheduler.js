852e4d375445dafa7135c5721f9eacc553edd55e
Melodie <fmsis@hotmail.com>
2018-11-03 23:19:50 -0600

    functionality 2

--- trainScheduler.js
+++ trainScheduler.js
@@ -8,62 +8,78 @@ var config = {
     messagingSenderId: "829029729915"
   };
   
-//Pull the config info and initialize Firebase
- firebase.initializeApp(config);
+  firebase.initializeApp(config);
 
-//Create a variable for the firebase database
-var database = firebase.database();
-
-//Create a click event to get the train info
-$("#add-train-btn").on("click", function(event) {
+  var database = firebase.database();
+  
+  // 2. Button for adding Employees
+  $("#add-train-btn").on("click", function(event) {
     event.preventDefault();
   
-//Create variables to hold the input data into an object
-var trainName = $("#train-name-input").val().trim();
-var trainDestination = $("#destination-input").val().trim();
-var firstTraintime = moment($("#train-time-input").val().trim(), 'HH,mm');
-var trainFrequency = $("frequency-input").val().trim();
-
-// Creates local "temporary" object for holding train info
-  var newTrain = {
-    name: trainName,
-    destination: trainDestination,
-    time: firstTraintime,
-    frequency: trainFrequency
-  };
-
-// Upload train info into the database
-database.ref().push(newTrain);
- 
-//Logs everything to console log
-//  console.log(newTrain.name);
-//   console.log(newTrain.destination);
-//   console.log(newTrain.firstTraintime);
-//   console.log(newTrain.trainFrequency);
-
-// //Create an alert to confirm the new train info was added
-// alert("New Train successfully added");
-
-//  // Clears all of the text-boxes
- $("#train-name-input").val("");
- $("#destination-input").val("");
- $("#train-time-input").val("");
- $("#frequency-input").val("");
-});
-
-// // Create Firebase event for adding a train to the database and a row in the html when a user adds an entry
- database.ref().on("child_added", function(childSnapshot) {
-  // console.log(childSnapshot.val());
-
-//     // Store everything into a variable.
+    // Grabs user input
+    var trainName = $("#train-name-input").val().trim();
+    var trainDestination = $("#destination-input").val().trim();
+    var firstTrainTime = ("#train-time-input").val().trim();
+    var trainFrequency = $("#frequency-input").val().trim();
+  
+    // Creates local "temporary" object for holding employee data
+    var newTrain = {
+      name: trainName,
+      destination: trainDestination,
+      firstTime: firstTrainTime,
+      frequency: trainFrequency
+    };
+  
+    // Uploads employee data to the database
+    database.ref().push(newTrain);
+  
+    // Logs everything to console
+    console.log(newTrain.name);
+    console.log(newTrain.destination);
+    console.log(newTrain.firstTime);
+    console.log(newTrain.frequency);
+  
+    alert("New Train successfully added");
+  
+    // Clears all of the text-boxes
+    $("#train-name-input").val("");
+    $("#destination-input").val("");
+    $("#train-time-input").val("");
+    $("#frequency-input").val("");
+  });
+  
+  // 3. Create Firebase event for adding trains to the database and a row in the html when a user adds an entry
+  database.ref().on("child_added", function(childSnapshot) {
+    console.log(childSnapshot.val());
+  
+    // Store everything into a variable.
     var trainName = childSnapshot.val().name;
     var trainDestination = childSnapshot.val().destination;
-    var firstTraintime = childSnapshot.val().time;
+    var firstTrainTime = childSnapshot.val().firstTime;
     var trainFrequency = childSnapshot.val().frequency;
-
-//   // Train Info
-  // console.log(trainName);
-  // console.log(trainDestination);
-  // console.log(firstTraintime);
-  // console.log(trainFrequency);
- });
+  
+    // Train Info
+    console.log(trainName);
+    console.log(trainDestination);
+    console.log(firstTrainTime);
+    console.log(trainFrequency);
+  
+    // Calculate next arrival variable
+    var nextArrival = ("3:30");
+  
+   //Calculate minutes away
+    var minutesAway = ("3");
+  
+    // Create the new row
+    var newRow = $("<tr>").append(
+      $("<td>").text(trainName),
+      $("<td>").text(trainDestination),
+      $("<td>").text(trainFrequency),
+      $("<td>").text(nextArrival),
+      $("<td>").text(minutesAway),
+    );
+  
+    // Append the new row to the table
+    $("#train-table > tbody").append(newRow);
+  });
+  
